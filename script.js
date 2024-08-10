d3f581973b79cb6fe025a289eb78b359e4d32318
Aayush Mishra <aayushmishra1105@gmail.com>
2024-08-11 00:39:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// Smooth Scroll
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Sticky Navbar
+window.onscroll = function() {
+    stickyNavbar();
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
